bf110c203cc6de5891add4663d87fc8ee3d30989
Leonardo Rivera <lrivera@oicr.on.ca>
2022-09-20 10:57:09 -0400

    file download

--- src/storage.ts
+++ src/storage.ts
@@ -6,33 +6,37 @@ const fs = require('fs');
 const path = require('path');
 
 const cwd = path.join(__dirname, '..');
-const destFileName = path.join(cwd, 'downloaded.txt');
 
 // Creates a client
 const storage = new Storage();
 
 export const streamFileDownload = async function (bucketName: string, fileName: string) {
-  // The example below demonstrates how we can reference a remote file, then
-  // pipe its contents to a local file.
-  // Once the stream is created, the data can be piped anywhere (process, sdout, etc)
-  await storage
-    .bucket(bucketName)
-    .file(fileName)
-    .createReadStream() //stream is created
-    .pipe(fs.createWriteStream(destFileName))
-    .on('finish', () => {
-      // The file download is complete
-      logger.debug(`gs://${bucketName}/${fileName} downloaded to ${destFileName}.`);
-    });
+  const destFileName = path.join(cwd, fileName.split('/').pop()?.replaceAll(':', '_'));
+  fs.access(destFileName, fs.F_OK, (err: string) => {
+    if (err) {
+      logger.info(`Downloading file:${fileName}`);
+      storage
+        .bucket(bucketName)
+        .file(fileName)
+        .createReadStream() //stream is created
+        .pipe(fs.createWriteStream(destFileName))
+        .on('finish', () => {
+          // The file download is complete
+          logger.info(`gs://${bucketName}/${fileName} downloaded to ${destFileName}.`);
+        });
+    }
+
+    //file exists
+    logger.info(`file:${destFileName} already exist`);
+  });
 };
 
 export const listFiles = async function (bucketName: string, folderName: string) {
-
-    const options = {
-        prefix: folderName || "",
-        autoPaginate: false,
-        delimiter: "/"
-    };
+  const options = {
+    prefix: folderName || '',
+    autoPaginate: false,
+    delimiter: '/',
+  };
 
   // Lists files in the bucket
   logger.debug(`List files on bucket ${bucketName} folder:${folderName}`);
