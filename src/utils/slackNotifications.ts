d73b1f8d69e80ee69305651aada969fb4adda337
Leonardo Rivera <leorivera_88@hotmail.com>
2022-12-16 17:25:26 -0500

    format slack messages

--- src/utils/slackNotifications.ts
+++ src/utils/slackNotifications.ts
@@ -9,11 +9,19 @@ export enum NOTIFICATION_CATEGORY_ICON {
   WARN = ':warning:',
 }
 
+// export type MessageKey = 'event' | 'time' | 'updated' | 'failed';
+export type MessageMap = {
+  event: string;
+  time: string;
+  analysisUpdated?: number;
+  error?: any;
+};
+
 export const sendSlackNotification = async ({
   message,
   category,
 }: {
-  message: Record<string, string>;
+  message: MessageMap;
   category: NOTIFICATION_CATEGORY_ICON;
 }): Promise<void> => {
   if (config.notifications?.slack_url) {
