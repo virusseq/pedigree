34ca651e9ae0418c973540750630827b0c7eb183
Leonardo Rivera <leorivera_88@hotmail.com>
2023-01-05 14:14:46 -0500

    revert Jenkins deployment

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -108,7 +108,6 @@ pipeline {
         anyOf {
           branch 'develop'
           branch 'main'
-          branch 'feature/slack-notifications'
         }
       }
       steps {
@@ -164,7 +163,6 @@ pipeline {
       when {
         anyOf {
           branch 'develop'
-          branch 'feature/slack-notifications'
         }
       }
       steps {
